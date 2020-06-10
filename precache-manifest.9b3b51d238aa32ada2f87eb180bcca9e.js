0aa9673d004fcead4b928c0a05ea9e9a8963d1d8
sayanbanerjee1999 <sb2131@cse.jgec.ac.in>
2020-06-11 03:11:40 +0530

    first

---
+++ precache-manifest.9b3b51d238aa32ada2f87eb180bcca9e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6eaa7784cfb5ef89ff72a9f352b5d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5368e17f25600eb67db",
+    "url": "/static/css/main.10b8f09f.chunk.css"
+  },
+  {
+    "revision": "2921ef0fddc42e5be997",
+    "url": "/static/js/2.1dc50f7b.chunk.js"
+  },
+  {
+    "revision": "f77df61ad5aee6f606510c5c8da8d179",
+    "url": "/static/js/2.1dc50f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5368e17f25600eb67db",
+    "url": "/static/js/main.ed281a14.chunk.js"
+  },
+  {
+    "revision": "3b9cf721881a456d023c",
+    "url": "/static/js/runtime-main.6677f8c6.js"
+  },
+  {
+    "revision": "4ecee4f1bee28e53dfa1f936de67e175",
+    "url": "/static/media/logo.4ecee4f1.PNG"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  }
+]);
